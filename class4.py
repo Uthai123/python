961b8bb24ef989498ddf594f830c0e65fcc71eda
Uthai123 <uthaimurali5@gmail.com>
2025-07-30 14:31:49 +0530

    python
    class 4

---
+++ class4.py
@@ -0,0 +1,66 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+================= RESTART: C:\Users\DELL\Music\python\class4.py ================
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17]
+
+================= RESTART: C:\Users\DELL\Music\python\class4.py ================
+Traceback (most recent call last):
+  File "C:\Users\DELL\Music\python\class4.py", line 12, in <module>
+    a.extend(18)
+TypeError: 'int' object is not iterable
+
+================= RESTART: C:\Users\DELL\Music\python\class4.py ================
+Traceback (most recent call last):
+  File "C:\Users\DELL\Music\python\class4.py", line 12, in <module>
+    a.extend(18,[19,12,13])
+TypeError: list.extend() takes exactly one argument (2 given)
+
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+Traceback (most recent call last):
+  File "C:\Users\DELL\Music\python\class4.py", line 12, in <module>
+    a.extend(18)
+TypeError: 'int' object is not iterable
+
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, '1', '8']
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 18]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 19, 12, 13]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+Traceback (most recent call last):
+  File "C:\Users\DELL\Music\python\class4.py", line 23, in <module>
+    a.insert(15,18,16,17)
+TypeError: insert expected 2 arguments, got 4
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 18]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 15]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 16]
+>>> 
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 17]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 13]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 20]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 14]
+>>> 
+=============== RESTART: C:\Users\DELL\Music\python\class4.py ===============
+[11, 12, 13, 14, 15, 16, 17, 18, 15]
