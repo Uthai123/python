4a47b5a350dfa6e616de04c6dfb0da1e457eaa7b
Uthai123 <uthaimurali5@gmail.com>
2025-07-30 14:34:07 +0530

    class 6

---
+++ class6.py
@@ -0,0 +1,32 @@
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','Name':'Mohana'}
+print(x)
+'''
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','Name':'Mohana'}
+print(type(x))
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','Name':'Mohana'}
+print(len(x))
+
+
+x['Name']='priya'
+print(x)
+
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','name':'Mohana'}
+x.update({'gender':'female','marital status':'married'})
+print('new updated dictionary is',x)
+'''
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','name':'Mohana'}
+print(x.keys())
+'''
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','name':'Mohana'}
+print(x.values())
+'''
+'''
+x={'Name':'Mohana','Age':35,'Degree':'M.tech','name':'Mohana'}
+print(x.items())
+'''
