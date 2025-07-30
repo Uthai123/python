2cc1cee2e62aafdbd69944cd1ad7b0ae6a7806b3
Uthai123 <uthaimurali5@gmail.com>
2025-07-30 14:28:59 +0530

    python class 1
    python class1

---
+++ class1.py
@@ -0,0 +1,66 @@
+'''
+a=13
+b=15
+print(a+b)
+
+
+
+a=17
+b=16
+print(a-b)
+
+#mul
+'''
+'''
+a=11
+b=10
+print(a*b)
+
+
+a=14
+b=12
+print(a/b)
+'''
+'''
+a=45
+b=46
+c=a+b
+print(c)
+
+a='murali'
+b='uthai'
+print(a+b)
+
+'''
+
+#type and length
+'''
+a=23
+b="suk una"
+c="56"
+print("saran mark: ",a)
+print(type(a))
+print(type(b))
+print(type(c))
+print(len(b))
+#print(len(a))
+print(len(c))
+'''
+'''
+f=8063
+print('value f is',f)
+print(type(f))      
+
+
+c=30+3j
+print('complex value is',c)
+print(type(c))
+f=80.45
+print(int(f))
+print(complex(f))
+
+'''
+
+
+
+
